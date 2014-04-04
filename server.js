8f39d9ad7b0bbdf37fefe1d3328db1d447d221c2
scc416300330 <416300330@qq.com>
2014-04-04 17:26:13 +0800

    111

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var http = require("http");
+var url = require("url");
 
-function onRequest(request, response) {
-	console.log("Request received");
-	response.writeHead(200, {"Content-Type":"text/plain"});
-	response.write("Hellow World");
-	response.end();
+function start(route) {
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for "+pathname+" received.");
+		
+		route(pathname);
+		
+		response.writeHead(200, {"Content-Type":"text/plain"});
+		response.write("Hellow World");
+		response.end();
+	}
+
+	http.createServer(onRequest).listen(8888);
+
+	console.log("Server has Started");
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log("Server has Started");
+exports.start = start;
